333276c33906939d143b3d3347955255d42c1730
Denys <denysborysenko479@gmail.com>
2025-11-10 18:08:07 +0200

    update index.js (change first name)

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const server = http.createServer((req, res) => {
        res.end(); //end the response
     }else{
        res.write('<h1>Hello World!<h1>'); //write a response
-       res.write('<h2>My name Maks<h2>'); //write a response
+       res.write('<h2>My name is Denys<h2>'); //write a response
        res.end(); //end the response
     }
 });
