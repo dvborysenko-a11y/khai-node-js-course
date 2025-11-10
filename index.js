f702599120effd6c901fd64f7f12f06fc372f924
Denys <denysborysenko479@gmail.com>
2025-11-10 18:03:31 +0200

    update index.js (add routing)

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const port = 3000; // Порт, на якому буде працювати се
 
 // Створення HTTP-сервера
 const server = http.createServer((req, res) => {
- res.statusCode = 200; // Код стану HTTP-відповіді
- res.end('Hello world!!!'); // Відправка відповіді
+   res.writeHead(200, {'Content-Type': 'text/html'}); // Повідомлюємо що формат буде HTML щоб браузер його відобразив
+   const url = req.url;
+    if(url ==='/about'){
+       res.write('<h1>about us page<h1>'); //write a response
+       res.end(); //end the response
+    }else if(url ==='/contact'){
+       res.write('<h1>contact us page<h1>'); //write a response
+       res.end(); //end the response
+    }else{
+       res.write('<h1>Hello World!<h1>'); //write a response
+       res.write('<h2>My name Maks<h2>'); //write a response
+       res.end(); //end the response
+    }
 });
 
 // Прослуховування порту та адреси сервера
