8800fc0ba7b78c8549e2e990d6ee90c5e5baccb5
Denys <denysborysenko479@gmail.com>
2025-11-25 21:17:31 +0200

    add first route

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+// handle get request
+app.get('/', (request, response) => {
+  // send back a response in plain text
+  response.send('response for GET request');
+});
+
 // start the server
-app.listen(3000, 
-   () => console.log('Server listening on port 3000.'));
+app.listen(3000, () => console.log(`server start at http://localhost:${port}/`));
