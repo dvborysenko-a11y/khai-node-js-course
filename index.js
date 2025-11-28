452efba9780cbe837412d58fdfefe2f723bd2d9e
Denys <denysborysenko479@gmail.com>
2025-11-28 16:59:09 +0200

    Refactor server: removed product routes and data, added imports

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
 const express = require('express');
+const productRoutes = require('./product.routes');
 
 const app = express();
-
-// Hardcoded in-memory products array (in a real app, data would typically come from a database)
-const products = [
-  { id: 1, name: 'Product 1', brand: 'Brand A' },
-  { id: 2, name: 'Product 2', brand: 'Brand B' },
-  { id: 3, name: 'Product 3', brand: 'Brand A' }
-];
-
 const port = 3000;
 
-// handle get request
-app.get('/', (request, response) => {
-  // send back a response in plain text
-  response.send('response for GET request');
-});
+app.use(productRoutes);
 
-// Route with a route parameter to get products by brand
-app.get('/products/:brand', (req, res) => {
-  const { brand } = req.params; // Access the brand parameter from the URL
-
-  // Filter products based on the brand parameter
-  const filteredProducts = products.filter(product => product.brand === brand);
-
-  res.json(filteredProducts); // Send the filtered products as a JSON response
-});
-
-// start the server
 app.listen(port, () => console.log(`server start at http://localhost:${port}/`));
