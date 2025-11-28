031b5b4c8a4cc2152d41d2e049f58b9d23ffc0ef
Denys <denysborysenko479@gmail.com>
2025-11-28 17:38:05 +0200

    Add: import middleware

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const productRoutes = require('./product.routes');
+const { logRequest } = require('./middleware');
 
 const app = express();
 const port = 3000;
 
+app.use(logRequest);
 app.use(productRoutes);
 
 app.listen(port, () => console.log(`server start at http://localhost:${port}/`));
