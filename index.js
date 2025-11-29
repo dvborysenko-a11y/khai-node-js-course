6e78e6b778629740bc7f775464221f1baba85219
Denys <denysborysenko479@gmail.com>
2025-11-29 15:29:02 +0200

    Update index: use errorResponder

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const productRoutes = require('./product.routes');
 const { logRequest } = require('./middleware');
+const { errorResponder } = require('./error.middleware');
 
 const app = express();
 const port = 3000;
 
 app.use(logRequest);
 app.use(productRoutes);
+app.use(errorResponder);
 
 app.listen(port, () => console.log(`server start at http://localhost:${port}/`));
