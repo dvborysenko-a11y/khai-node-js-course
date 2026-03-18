bcabe9bea6dab3c7819185cc783dd5aa8c972580
Denys <denysborysenko479@gmail.com>
2026-03-18 15:08:39 +0200

    fix: remove redundant dialect field

--- drizzle.config.js
+++ drizzle.config.js
@@ -2,7 +2,6 @@ export default {
    schema: './src/db/schema.js',
    out: './drizzle',
    driver: "pg",
-   dialect: 'postgresql',
    dbCredentials: {
        host: '127.0.0.1',
        port: 5432,
