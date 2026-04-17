874ee8c09e24d840283270e9ffe1a883bde9b315
Denys <denysborysenko479@gmail.com>
2026-04-17 15:03:45 +0300

    chore: add user module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const productRoutes = require('./product.routes');
+const userRouter = require('./user/user.router');
 const { logRequest } = require('./middleware');
 const { errorResponder } = require('./error.middleware');
 
@@ -8,6 +9,7 @@ const port = 3000;
 
 app.use(logRequest);
 app.use(productRoutes);
+app.use(userRouter);
 app.use(errorResponder);
 
 app.listen(port, () => console.log(`server start at http://localhost:${port}/`));
